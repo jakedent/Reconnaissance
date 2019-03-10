f880c9df47f13fb0848383f3e4c30efbcb6b2a26
Jacob <info@jacobdent.com>
2019-03-10 18:59:33 +0000

    Update remote_host.py

--- remote_host.py
+++ remote_host.py
@@ -3,8 +3,11 @@ import subprocess
 
 def find_hosts():
     target = input("Enter IP : ")
-    # NSE script for finding hosts on remote IP.
-    subprocess.call(['nmap', '-p', '80', '--script', 'hostmap-bfk.nse', target])
+    try:
+        # NSE script for finding hosts on remote IP.
+        subprocess.call(['nmap', '-p', '80', '--script', 'hostmap-bfk.nse', target])
+    except Exception as e:
+        print("\nUnable to find hosts... {0}".format(e))
 
 
 if __name__ == '__main__':
