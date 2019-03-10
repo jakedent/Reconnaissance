1b5cd62f361ba9eb0e5fe8534ecedc3320487675
Jacob <info@jacobdent.com>
2019-03-10 18:55:43 +0000

    Update geo_locator.py

--- geo_locator.py
+++ geo_locator.py
@@ -4,8 +4,11 @@ import subprocess
 def g_locate():
     # NSE script for tracing IP geo-location.
     target = input("\nTarget URL / IP : ")
-    print("\nTracing geo-location of : ", target)
-    subprocess.call(['nmap', '--traceroute', '--script', 'traceroute-geolocation.nse', '-p', '80', target])
+    try:
+        print("\nTracing geo-location of : ", target)
+        subprocess.call(['nmap', '--traceroute', '--script', 'traceroute-geolocation.nse', '-p', '80', target])
+    except Exception as e:
+        print("\nUnable to trace geo-location. {0}".format(e))
 
 
 if __name__ == '__main__':
