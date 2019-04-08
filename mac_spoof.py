e284db70a5608b5ab7f5cd463f4d66adea643c00
Jacob <info@jacobdent.com>
2019-04-08 20:40:36 +0100

    Update mac_spoof.py

--- mac_spoof.py
+++ mac_spoof.py
@@ -1,5 +1,22 @@
-# spoof MAC address LINUX
+# generate and spoof MAC address LINUX
 import subprocess
+import random
+
+
+def mac_gen():
+    # print("\nGenerating Random MAC address...")
+    try:
+        global new_mac
+        new_mac = ("44:d1:01:%02x:%02x:%02x" % (
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255),
+        ))
+        # print("\nGenerated : ", new_mac)
+    except Exception as e:
+        print("\nUnable to generate new MAC address. {0}".format(e))
+
+    # print("\nDone!")
 
 
 def mac_func():
@@ -11,16 +28,16 @@ def mac_func():
         return
     elif mac_select == "wlan0":
         subprocess.call("ifconfig wlan0 down", shell=True)
-        subprocess.call("ifconfig wlan0 hw ether 44:d1:01:52:74:48", shell=True)
+        subprocess.call("ifconfig wlan0 hw ether " + new_mac, shell=True)
         subprocess.call("ifconfig wlan0 up", shell=True)
     elif mac_select == "eth0":
         subprocess.call("ifconfig eth0 down", shell=True)
-        subprocess.call("ifconfig eth0 hw ether 44:d1:01:52:74:48", shell=True)
+        subprocess.call("ifconfig eth0 hw ether " + new_mac, shell=True)
         subprocess.call("ifconfig eth0 up", shell=True)
     elif mac_select == "reset wlan0":
         old_wlan = input("Insert old wireless mac address : ")
         subprocess.call("ifconfig wlan0 down", shell=True)
-        subprocess.call("ifconfig wlan0 hw ether" + old_wlan, shell=True)
+        subprocess.call("ifconfig wlan0 hw ether " + old_wlan, shell=True)
         subprocess.call("ifconfig wlan0 up", shell=True)
     elif mac_select == "reset eth0":
         old_eth = input("Insert old wired mac address : ")
@@ -32,4 +49,5 @@ def mac_func():
 
 
 if __name__ == '__main__':
+    mac_gen()
     mac_func()
